ad258aabb399bd237d14b301f593bc96518104f2
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-14 15:05:41 -0400

    HW 2 Complete

---
+++ solution-hw3/js/calculation.js
@@ -0,0 +1,18 @@
+let glazing = ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"];
+let name = glazing[""];
+let glazingChoice = glazing.length;
+
+for (let i = 0; i < glazing.length; i++)
+{
+let name = glazing[i];
+console.log(name);
+}
+
+function glazingChange(add) {
+    // get value of selected glazing option
+    let DetailspriceChange = document.querySelector('.add');
+    const priceChange = add.value;
+    
+  // add your code to do update the price ...
+  }
+  
