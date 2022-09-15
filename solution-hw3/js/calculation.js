0794069cd952f54b9ea9d0050188f78875344faf
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-14 23:17:10 -0400

    HW 3

--- solution-hw3/js/calculation.js
+++ solution-hw3/js/calculation.js
@@ -1,16 +1,24 @@
-let glazing = ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"];
-let name = glazing[""];
-let glazingChoice = glazing.length;
+let basePrice = [2.75];
+let glazingPrice = [0, 0, 0.5, 1.5];
+let packPrice = [1, 3, 5, 10];
 
-for (let i = 0; i < glazing.length; i++)
-{
-let name = glazing[i];
-console.log(name);
+if (glazing == [0]) {
+    rollPrice = basePrice + 0;
 }
+if (glazing == [1]) {
+    rollPrice = basePrice + 0;
+}
+if (glazing == [2]) {
+    rollPrice = basePrice + 0.5;
+}
+if (glazing == [3]) {
+    rollPrice = basePrice + 1.5;
+}
+console.log(rollPrice);
 
 function glazingChange(add) {
     // get value of selected glazing option
-    let DetailspriceChange = document.querySelector('.add');
+    let DetailsPriceChange = document.querySelector('.add');
     const priceChange = add.value;
     
   // add your code to do update the price ...
