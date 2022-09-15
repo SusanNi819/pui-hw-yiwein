dd00c4fffc89961e11d7d72a41aed32518cf8f81
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-15 18:36:02 -0400

    HW 3

---
+++ solution-hw3/js/price.js
@@ -0,0 +1,21 @@
+let basePrice = 2.75;
+let glazingPrice = [0, 0, 0.5, 1.5];
+packPrice = [1, 3, 5, 10];
+
+function glazingChange(element) {
+  glazingPrice.glazingChange = d;
+    // get value of selected glazing option
+    const priceChange = element.value;
+    
+  // add your code to do update the price ...
+  }
+  
+  let updatePrice = document.querySelector('glazing');
+  updatePrice.onchange = glazingChange;
+
+  function packChange(element) {
+    // get value of selected glazing option
+    const priceChange = element.value;
+    
+  // add your code to do update the price ...
+  }
