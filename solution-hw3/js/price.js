8ea250c5a22bc011ab44e9a9d859b6ff2c2a1990
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-16 16:01:01 -0400

    HW 3

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -3,21 +3,64 @@ let glazingPrice = [0, 0, 0.5, 1.5];
 let packPrice = [1, 3, 5, 10];
 let glazing = document.querySelector(".glazing");
 let packSize = document.querySelector(".pack-size");
+let updatePrice = document.querySelector('.update-price');
 
 function glazingChange(element) {
-  glazingPrice.glazingChange = d;
-    // get value of selected glazing option
-    const priceChange = element.value;
-    
-  // add your code to do update the price ...
+  // get value of selected glazing option
+  const priceChange = element.glazing;
+  if (priceChange == 'Keep original') {
+    updatePrice = basePrice + glazingPrice[0];
+    const packPriceChange = element.packPrice;
+    if (packPriceChange == 1) {
+      updatePrice = '$ ' + priceChange * packPrice[0];
+  }   else if (packPriceChange == 3) {
+      updatePrice = '$ ' + priceChange * packPrice[1];
+  }   else if (packPriceChange == 5) {
+      updatePrice = '$ ' + priceChange * packPrice[2];
+  }   else if (packPriceChange == 10) {
+      updatePrice = '$ ' + priceChange * packPrice[3];
   }
-  
-  let updatePrice = document.querySelector('glazing');
-  updatePrice.onchange = glazingChange;
+  }
 
-  function packChange(element) {
-    // get value of selected glazing option
-    const priceChange = element.value;
-    
-  // add your code to do update the price ...
+  if (priceChange == 'Sugar milk') {
+    updatePrice = basePrice + glazingPrice[1];
+    const packPriceChange = element.packPrice;
+    if (packPriceChange == 1) {
+      updatePrice = '$ ' + priceChange * packPrice[0];
+  }   else if (packPriceChange == 3) {
+      updatePrice = '$ ' + priceChange * packPrice[1];
+  }   else if (packPriceChange == 5) {
+      updatePrice = '$ ' + priceChange * packPrice[2];
+  }   else if (packPriceChange == 10) {
+      updatePrice = '$ ' + priceChange * packPrice[3];
   }
+  }
+    
+  if (priceChange == 'Vanilla milk') {
+    updatePrice = basePrice + glazingPrice[2];
+    const packPriceChange = element.packPrice;
+    if (packPriceChange == 1) {
+      updatePrice = '$ ' + priceChange * packPrice[0];
+  }   else if (packPriceChange == 3) {
+      updatePrice = '$ ' + priceChange * packPrice[1];
+  }   else if (packPriceChange == 5) {
+      updatePrice = '$ ' + priceChange * packPrice[2];
+  }   else if (packPriceChange == 10) {
+      updatePrice = '$ ' + priceChange * packPrice[3];
+  }
+  }
+
+  if (priceChange == 'Double chocolate') {
+    updatePrice = basePrice + glazingPrice[3];
+    const packPriceChange = element.packPrice;
+    if (packPriceChange == 1) {
+      updatePrice = '$ ' + priceChange * packPrice[0];
+  }   else if (packPriceChange == 3) {
+      updatePrice = '$ ' + priceChange * packPrice[1];
+  }   else if (packPriceChange == 5) {
+      updatePrice = '$ ' + priceChange * packPrice[2];
+  }   else if (packPriceChange == 10) {
+      updatePrice = '$ ' + priceChange * packPrice[3];
+  }
+  }
+}
