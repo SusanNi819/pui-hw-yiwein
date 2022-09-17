12e9f78e62fa1a8941c7780ca194766f97963514
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-17 19:38:11 -0400

    HW 3

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -3,10 +3,11 @@ let glazingPrice = [0, 0, 0.5, 1.5];
 let packPrice = [1, 3, 5, 10];
 let updatePrice = 2.49;
 
+document.addEventListener("change", glazingChange());
 function glazingChange(element) {
-  let element1 = document.querySelector(".glazing");
+  let element1 = document.querySelector("#glazing");
   // get value of selected glazing option
-  const priceChange = element.glazing;
+  const priceChange = document.querySelector("#glazing");
   if (priceChange == 'Keep original') {
     updatePrice = (basePrice + glazingPrice[0]) * packPrice[0];
   } else if (priceChange == 'Sugar milk') {
@@ -16,11 +17,13 @@ function glazingChange(element) {
   } else if (priceChange == 'Double chocolate') {
     updatePrice = (basePrice + glazingPrice[3]) * packPrice[0];
   }
+  document.getElementById("updatedPrice").innerHTML = '$ ' + updatePrice;
 }
 
+document.addEventListener('change', packChange());
 function packChange(element) {
-  let element2 = document.querySelector(".pack-size");
-  const packPriceChange = element.pack-size;
+  let element2 = document.querySelector("#pack-size");
+  const packPriceChange = document.querySelector("#pack-size");
   if (packPriceChange == 1) {
     updatePrice = updatePrice * packPrice[0];
   } else if (packPriceChange == 3) {
@@ -30,7 +33,5 @@ function packChange(element) {
   } else if (packPriceChange == 12) {
     updatePrice = updatePrice * packPrice[3];
   }
-  element.innerHTML = "$ " + updatePrice;
+  document.getElementById("updatedPrice").innerHTML = '$ ' + updatePrice;
 }
-document.querySelector(".updatedPrice").addEventListener('onChange', glazingChange)
-document.querySelector(".updatedPrice").addEventListener('onChange', packChange)
