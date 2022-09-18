7e5a56969b419c64904c036404ccbbf1e04c6fb4
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-17 21:16:45 -0400

    HW 3

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -3,11 +3,10 @@ let glazingPrice = [0, 0, 0.5, 1.5];
 let packPrice = [1, 3, 5, 10];
 let updatePrice = 2.49;
 
-document.addEventListener("change", glazingChange());
+document.getElementById("glazing").addEventListener('change', glazingChange);
 function glazingChange(element) {
-  let element1 = document.querySelector("#glazing");
-  // get value of selected glazing option
-  const priceChange = document.querySelector("#glazing");
+  let element1 = document.getElementById("glazing");
+  let priceChange = element1;
   if (priceChange == 'Keep original') {
     updatePrice = (basePrice + glazingPrice[0]) * packPrice[0];
   } else if (priceChange == 'Sugar milk') {
@@ -18,12 +17,13 @@ function glazingChange(element) {
     updatePrice = (basePrice + glazingPrice[3]) * packPrice[0];
   }
   document.getElementById("updatedPrice").innerHTML = '$ ' + updatePrice;
-}
 
-document.addEventListener('change', packChange());
+ }
+ 
+document.getElementById("pack-size").addEventListener('change', packChange);
 function packChange(element) {
-  let element2 = document.querySelector("#pack-size");
-  const packPriceChange = document.querySelector("#pack-size");
+  let element2 = document.getElementById("pack-size");
+  let packPriceChange = element2;
   if (packPriceChange == 1) {
     updatePrice = updatePrice * packPrice[0];
   } else if (packPriceChange == 3) {
