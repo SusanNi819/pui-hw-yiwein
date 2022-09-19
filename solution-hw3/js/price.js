f2682467bf509f99d0544ae999d9804d3dd10878
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-19 13:58:15 -0400

    HW 3 Completed

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -2,37 +2,38 @@ let basePrice = 2.49;
 let glazingPrice = [0, 0, 0.5, 1.5];
 let packPrice = [1, 3, 5, 10];
 let updatePrice = 2.49;
+let finalPrice = 0;
+let priceChange = 0;
+let packPriceChange = 1;
 
 document.getElementById("glazing").addEventListener('change', glazingChange);
 function glazingChange(element) {
   let element1 = document.getElementById("glazing");
-  let priceChange = element1;
+  priceChange = element1.value;
   if (priceChange == "Keep original") {
-    updatePrice = (basePrice + glazingPrice[0]) * packPrice[0];
+    updatePrice = (basePrice + glazingPrice[0]) * packPriceChange;
   } else if (priceChange == "Sugar milk") {
-    updatePrice = (basePrice + glazingPrice[1]) * packPrice[0];
+    updatePrice = (basePrice + glazingPrice[1]) * packPriceChange;
   } else if (priceChange == "Vanilla milk") {
-    updatePrice = (basePrice + glazingPrice[2]) * packPrice[0];
+    updatePrice = (basePrice + glazingPrice[2]) * packPriceChange;
   } else if (priceChange == "Double chocolate") {
-    updatePrice = (basePrice + glazingPrice[3]) * packPrice[0];
-    console.log('here');
+    updatePrice = (basePrice + glazingPrice[3]) * packPriceChange;
   }
-  document.getElementById("updatedPrice").innerHTML = '$ ' + updatePrice;
-
+  document.getElementById("updatedPrice").innerHTML = '$ ' + Math.round(updatePrice*100)/100;
  }
  
 document.getElementById("pack-size").addEventListener('change', packChange);
 function packChange(element) {
   let element2 = document.getElementById("pack-size");
-  let packPriceChange = element2;
+  packPriceChange = element2.value;
   if (packPriceChange == "1") {
-    updatePrice = updatePrice * packPrice[0];
+    finalPrice = updatePrice * packPrice[0];
   } else if (packPriceChange == "3") {
-    updatePrice = updatePrice * packPrice[1];
-  } else if (packPriceChange == "6") {
-    updatePrice = updatePrice * packPrice[2];
-  } else if (packPriceChange == "12") {
-    updatePrice = updatePrice * packPrice[3];
+    finalPrice = updatePrice * packPrice[1];
+  } else if (packPriceChange == "5") {
+    finalPrice = updatePrice * packPrice[2];
+  } else if (packPriceChange == "10") {
+    finalPrice = updatePrice * packPrice[3];
   } 
-  document.getElementById("updatedPrice").innerHTML = '$ ' + updatePrice;
+  document.getElementById("updatedPrice").innerHTML = '$ ' + Math.round(finalPrice*100)/100;
 }
