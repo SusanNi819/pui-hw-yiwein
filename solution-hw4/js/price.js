f8530da4052f97456e7383b59887cc097bbd985d
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-22 22:12:02 -0400

    HW 4

--- solution-hw4/js/price.js
+++ solution-hw4/js/price.js
@@ -1,25 +1,76 @@
 let glazingPrice = [0, 0, 0.5, 1.5];
 let packPrice = [1, 3, 5, 10];
-let basePrice = [2.49, 3.49, 2.99, 3.49, 3.99, 3.99];
-let baseUpdate = 2.49;
+let basePrice = 2.49;
 let glazingUpdate = 0;
 let packUpdate = 1;
 
+let allGlazing = [
+  {
+    name: "Keep original",
+    value: 2.49,
+  },
+  {
+    name: "Sugar milk",
+    value: 2.49,
+  },
+  {
+    name: "Vanilla milk",
+    value: 2.99,
+  },
+  {
+    name: "Double chocolate",
+    value: 3.99,
+  },
+];
+
+let allPackSize = [
+  {
+    name: "1",
+    value: 1,
+  },
+  {
+    name: "3",
+    value: 3,
+  },
+  {
+    name: "6",
+    value: 5,
+  },
+  {
+    name: "12",
+    value: 10,
+  },
+];
+
+for (let i = 0; i < allGlazing.length; i++) {
+  const select = document.createElement ("option");
+  select.value = allGlazing[i].value;
+  select.innerHTML = allGlazing[i].name;
+  document.getElementById("glazing").appendChild(select);
+}
+
+for (let i = 0; i < allPackSize.length; i++) {
+  const select = document.createElement ("option");
+  select.value = allPackSize[i].value;
+  select.innerHTML = allPackSize[i].name;
+  document.getElementById("pack-size").appendChild(select);
+}
+
 function updatePrice() {
-  return (baseUpdate + glazingUpdate) * packUpdate;
+  return (basePrice + glazingUpdate) * packUpdate;
 }
 
 document.getElementById("glazing").addEventListener('change', glazingChange);
 function glazingChange(element) {
   let element1 = document.getElementById("glazing");
   priceChange = element1.value;
-  if (priceChange == "Keep original") {
-    glazingUpdate = glazingPrice[0]
-  } else if (priceChange == "Sugar milk") {
+  if (priceChange == allGlazing[0].value) {
+    glazingUpdate = glazingPrice[0];
+  } else if (priceChange == allGlazing[1].value) {
     glazingUpdate = glazingPrice[1];
-  } else if (priceChange == "Vanilla milk") {
+  } else if (priceChange == allGlazing[2].value) {
     glazingUpdate = glazingPrice[2];
-  } else if (priceChange == "Double chocolate") {
+  } else if (priceChange == allGlazing[3].value) {
     glazingUpdate = glazingPrice[3];
   }
   const newPrice = updatePrice()
@@ -30,13 +81,13 @@ document.getElementById("pack-size").addEventListener('change', packChange);
 function packChange(element) {
   let element2 = document.getElementById("pack-size");
   packPriceChange = element2.value;
-  if (packPriceChange == "1") {
+  if (packPriceChange == allPackSize[0].value) {
     packUpdate = packPrice[0];
-  } else if (packPriceChange == "3") {
+  } else if (packPriceChange == allPackSize[1].value) {
     packUpdate = packPrice[1];
-  } else if (packPriceChange == "5") {
+  } else if (packPriceChange == allPackSize[2].value) {
     packUpdate = packPrice[2];
-  } else if (packPriceChange == "10") {
+  } else if (packPriceChange == allPackSize[3].value) {
     packUpdate = packPrice[3];
   } 
   const newPrice = updatePrice()
