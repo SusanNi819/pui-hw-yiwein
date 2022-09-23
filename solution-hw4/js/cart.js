110be0dad5ff9a81c7b405827830884a6429e003
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-23 15:47:08 -0400

    HW 4

---
+++ solution-hw4/js/cart.js
@@ -0,0 +1,10 @@
+let cart = [
+    class Roll {
+        constructor(rollType, rollGlazing, packSize, basePrice) {
+            this.type = rollType;
+            this.glazing =  rollGlazing;
+            this.size = packSize;
+            this.basePrice = basePrice;
+        }
+    }    
+]
