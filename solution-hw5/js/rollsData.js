84f394849a0b4f43558931eb878458c246b2e096
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-28 16:32:35 -0400

    HW 5

---
+++ solution-hw5/js/rollsData.js
@@ -0,0 +1,36 @@
+const rolls = {
+    "Original": {
+        "rollName":"Original cinnamon roll",
+        "basePrice": 2.49,
+        "imageFile": "Assets/original-cinnamon-roll.jpeg"
+    },
+    "Apple": {
+        "rollName":"Apple cinnamon roll",
+        "basePrice": 3.49,
+        "imageFile": "Assets/apple-cinnamon-roll.jpeg"
+    },
+    "Raisin": {
+        "rollName":"Raisin cinnamon roll",
+        "basePrice": 2.99,
+        "imageFile": "Assets/raisin-cinnamon-roll.jpeg"
+    },
+    "Walnut": {
+        "rollName":"Walnut cinnamon roll",
+        "basePrice": 3.49,
+        "imageFile": "Assets/walnut-cinnamon-roll.jpeg"
+    },
+    "Double-Chocolate": {
+        "rollName":"Double Chocolate cinnamon roll",
+        "basePrice": 3.99,
+        "imageFile": "Assets/double-chocolate-cinnamon-roll.jpeg"
+    },
+    "Strawberry": {
+        "rollName":"Strawberry cinnamon roll",
+        "basePrice": 3.99,
+        "imageFile": "Assets/strawberry-cinnamon-roll.jpeg"
+    }    
+};
+
+
+
+
