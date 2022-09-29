bada2f2442e2bff44120ae288c163bc5955ade66
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-29 19:48:58 -0400

    HW 5

--- solution-hw5/js/roll.js
+++ solution-hw5/js/roll.js
@@ -3,9 +3,9 @@ const params = new URLSearchParams(queryString);
 const choseRoll = params.get('rolls');
 const rollChoseName = rolls[choseRoll];
 
-const rollNames = rollChoseName.rollName;
+const rollNames = rollChoseName.rollKeyName;
 const rollNameElement = document.querySelector('#catchline');
-rollNameElement.innerText = rollNames;
+rollNameElement.innerText = rollNames + " Cinnamon Roll";
 
 const rollPrices = rollChoseName.basePrice;
 const rollPriceElement = document.querySelector('#updatedPrice');
@@ -14,6 +14,7 @@ rollPriceElement.innerText = "$ " + rollPrices;
 const rollImages = rollChoseName.imageFile;
 const rollImageElement = document.querySelector('#cinnamon-roll-image');
 rollImageElement.src = rollImages;
+
 
 
 
