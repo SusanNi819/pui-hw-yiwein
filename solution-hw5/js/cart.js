3ed6e23b320fa458d607734428764227d1257d31
SusanNi819 <86569426+SusanNi819@users.noreply.github.com>
2022-09-30 16:46:17 -0400

    HW 5

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -55,13 +55,141 @@ function createElement(cartItem) {
     itemSet.delete(cartItem);
   }
 
+  let rollNames = [
+    {
+      name: "Original",
+      value: 2.49
+    },
+    {
+      name: "Apple",
+      value: 3.49
+    },
+    {
+      name: "Raisin",
+      value: 2.99
+    },
+    {
+      name: "Walnut",
+      value: 3.49
+    },
+    {
+      name: "Double-Chocolate",
+      value: 3.99
+    },
+    {
+      name: "Strawberry",
+      value: 3.99
+    },
+
+  ]
+
+  let glazingNames = [
+    {
+      name: "Original",
+      value: 0,
+    },
+    {
+      name: "Sugar Milk",
+      value: 0,
+    },
+    {
+      name: "Vanilla Milk",
+      value: 0.5,
+    },
+    {
+      name: "Double Chocolate",
+      value: 1.5,
+    },
+  ];
+  
+  let packSizes = [
+    {
+      name: "1",
+      value: 1,
+    },
+    {
+      name: "3",
+      value: 3,
+    },
+    {
+      name: "6",
+      value: 5,
+    },
+    {
+      name: "12",
+      value: 10,
+    },
+  ];
+
+  function updateCalculatedPrice(baseUpdate, glazingUpdate, packUpdate) {
+    return (baseUpdate + glazingUpdate) * packUpdate;
+  }
+
+  console.log("Update calculated price")
+  console.log(updateCalculatedPrice(2.49, 0.5, 6))
+  // 2.49 -> baseUpdate, 0.5 -> glazingUpdate, 6 -> packUpdate
+
+  function newFunction(a, b, c) {
+    return (a + b) * c;
+  }
+
+ console.log(newFunction(1, 2, 3));
+
+  function baseChange(rollType) {
+    let baseUpdate = 2.49;
+    if (rollType == rollNames[0].name) {
+      baseUpdate = rollNames[0].value;
+    } else if (rollType == rollNames[1].name) {
+      baseUpdate = rollNames[1].value;
+    } else if (rollType == rollNames[2].name) {
+      baseUpdate = rollNames[2].value;
+    } else if (rollType == rollNames[3].name) {
+      baseUpdate = rollNames[3].value;
+    } else if (rollType == rollNames[4].name) {
+      baseUpdate = rollNames[4].value;
+    } else if (rollType == rollNames[5].name) {
+      baseUpdate = rollNames[5].value;
+    } 
+
+
+    return baseUpdate
+  }
+
+  function packChange(packSize) {
+    let packUpdate = 0;
+    if (packSize == packSizes[0].name) {
+      packUpdate = packSizes[0].value;
+    } else if (packSize == packSizes[1].name) {
+      packUpdate = packSizes[1].value;
+    } else if (packSize == packSizes[2].name) {
+      packUpdate = packSizes[2].value;
+    } else if (packSize == packSizes[3].name) {
+      packUpdate = packSizes[3].value;
+    } 
+    return packUpdate
+  }
+
+  function glazeChange(rollGlazing) {
+    let glazingUpdate = 1;
+    if (rollGlazing == glazingNames[0].name) {
+      glazingUpdate = glazingNames[0].value;
+    } else if (rollGlazing == glazingNames[1].name) {
+      glazingUpdate = glazingNames[1].value;
+    } else if (rollGlazing== glazingNames[2].name) {
+      glazingUpdate = glazingNames[2].value;
+    } else if (rollGlazing == glazingNames[3].name) {
+      glazingUpdate = glazingNames[3].value;
+    } 
+    return glazingUpdate
+  }
+
   const cartItemOne = addToCart(
     "Assets/original-cinnamon-roll.jpeg",
     "Original",
     "Sugar Milk",
     1,
     2.49,
-    0
+    updateCalculatedPrice(baseChange("Original"), glazeChange("Sugar Milk"), packChange("1")).toFixed(2)
   );
 
 const cartItemTwo = addToCart(
@@ -70,7 +198,7 @@ const cartItemTwo = addToCart(
     "Vanilla Milk",
     12,
     3.49,
-    0
+    updateCalculatedPrice(baseChange("Walnut"), glazeChange("Vanilla Milk"), packChange("12")).toFixed(2)
   );
 
 const cartItemThree = addToCart(
@@ -79,7 +207,7 @@ const cartItemThree = addToCart(
     "Sugar Milk",
     3,
     2.99,
-    0
+    updateCalculatedPrice(baseChange("Raisin"), glazeChange("Sugar Milk"), packChange("3")).toFixed(2)
   );
 
 const cartItemFour = addToCart(
@@ -88,7 +216,7 @@ const cartItemFour = addToCart(
     "Original",
     3,
     3.49,
-    0
+    updateCalculatedPrice(baseChange("Apple"), glazeChange("Original"), packChange("3")).toFixed(2)
   );
 
   for (const cartItem of itemSet) {
